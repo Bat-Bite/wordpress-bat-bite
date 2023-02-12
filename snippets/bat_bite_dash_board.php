cb34d226298f76892285b38abbf761039d2e7330
RoeZX <spsandoval94@gmail.com>
2023-02-12 18:33:42 -0300

    up

--- snippets/bat_bite_dash_board.php
+++ snippets/bat_bite_dash_board.php
@@ -1,38 +1,40 @@
 <?php
+
 /**
  *
  * @return void
  */
-function changelog_widget_callback():void {
- 
-    $changelog= [
+function changelog_widget_callback(): void
+{
 
-    ];
+    $changelog = [];
     $separator = ' - ';
-    
+
     /**
      *
      * @return void
      */
-        function listarArray(array $array, string $separator): void {
-            echo '<div class="main"><ul>';
-            foreach($array as $key => $value) {
-                echo '<li>'.esc_html($key).esc_html($separator).esc_html($value).'</li>';
-            }
-            echo '</ul></div>';
+    function listarArray(array $array, string $separator): void
+    {
+        echo '<div class="main"><ul>';
+        foreach ($array as $key => $value) {
+            echo '<li>' . esc_html($key) . esc_html($separator) . esc_html($value) . '</li>';
         }
-        
-        listarArray($changelog, $separator);
+        echo '</ul></div>';
     }
-    
-    add_action( 'wp_dashboard_setup', 'add_changelog_widget' );
-    
-    function add_changelog_widget() {
-        if (current_user_can('editor' || 'admin')) {
-            wp_add_dashboard_widget(
-                'changelog_widget',
-                'Bat-Bite - Cambios',
-                'changelog_widget_callback'
-            );
-        }
+
+    listarArray($changelog, $separator);
+}
+
+add_action('wp_dashboard_setup', 'add_changelog_widget');
+
+function add_changelog_widget()
+{
+    if (current_user_can('editor' || 'admin')) {
+        wp_add_dashboard_widget(
+            'changelog_widget',
+            'Bat-Bite - Cambios',
+            'changelog_widget_callback'
+        );
     }
+}
--- snippets/bat_bite_link_button_shortcode.php
+++ snippets/bat_bite_link_button_shortcode.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Shortcode para mostrar un botón con un enlace sanitizado que se encuentra en ACF.
  *
@@ -13,9 +14,9 @@ function bat_bite_link_button_shortcode(): string
     if (!$link) {
         return '';
     }
-    
+
     return '
-    <div class="'.$elementor_class.'">
+    <div class="' . $elementor_class . '">
         <div class="elementor-widget-container">
             <div class="elementor-button-wrapper"> 
                 <a target="_blank" rel="noopener noreferrer follow" href="' . $link . '" class="elementor-button-link elementor-button elementor-size-sm" role="button">
