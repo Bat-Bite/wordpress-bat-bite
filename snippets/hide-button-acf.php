94c2c563f713757970c97b38c6cf7304ea603e9c
RoeZX <spsandoval94@gmail.com>
2023-02-12 18:20:45 -0300

    Shortcode para mostrar un botón con un enlace sanitizado que se encuentra en ACF

--- snippets/hide-button-acf.php
+++ snippets/hide-button-acf.php
@@ -4,25 +4,28 @@
  *
  * @return string
  */
-function link_button_shortcode(): string {
-    $acf_link = get_field('link');
+function bat_bite_link_button_shortcode(): string
+{
+    $link = esc_url(get_field(''));
+    $elementor_class=sanitize_text_field('');
+    $text = sanitize_text_field('');
 
-if (!$acf_link && !filter_var($acf_link, FILTER_VALIDATE_URL)) {
-    return '';
+    if (!$link) {
+        return '';
+    }
+    
+    return '
+    <div class="'.$elementor_class.'">
+        <div class="elementor-widget-container">
+            <div class="elementor-button-wrapper"> 
+                <a target="_blank" rel="noopener noreferrer follow" href="' . $link . '" class="elementor-button-link elementor-button elementor-size-sm" role="button">
+                    <span class="elementor-button-content-wrapper">
+                        <span class="elementor-button-text">' . $text . '</span>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>';
 }
 
-return '
-<div class="tu clase de elementor">
-<div class="elementor-widget-container">
-<div class="elementor-button-wrapper"> 
- <a target="_blank" rel="noopener noreferrer follow" href="' . esc_url($acf_link) . '" class="elementor-button-link elementor-button elementor-size-sm" role="button">
-  <span class="elementor-button-content-wrapper">
-  <span class="elementor-button-text">texto boton</span>
-  </span>
- </a>
-</div>
-</div>
-</div>';
-}
-
-add_shortcode('link_button', 'link_button_shortcode');
+add_shortcode('bat_bite_link_button', 'bat_bite_link_button_shortcode');
