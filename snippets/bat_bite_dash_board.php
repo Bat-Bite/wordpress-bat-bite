a920e463151fda66602d283eeb599906c5c9c862
RoeZX <spsandoval94@gmail.com>
2023-04-12 15:15:56 -0300

    refacto: cambiar foreach por array walk y mejoras min

--- snippets/bat_bite_dash_board.php
+++ snippets/bat_bite_dash_board.php
@@ -1,40 +1,33 @@
 <?php
 
-/**
- *
- * @return void
- */
-function changelog_widget_callback(): void
+function changelog_widget_callback():void 
 {
-
-    $changelog = [];
-    $separator = ' - ';
-
-    /**
-     *
-     * @return void
-     */
-    function listarArray(array $array, string $separator): void
-    {
-        echo '<div class="main"><ul>';
-        foreach ($array as $key => $value) {
-            echo '<li>' . esc_html($key) . esc_html($separator) . esc_html($value) . '</li>';
-        }
+	$changelog= [];
+	$separator = ' - ';
+	
+	function listarArray(array $array, string $separator): void
+	{
+		
+		echo '<div class="main"><ul>';
+		array_walk($array, function(&$value, $key) use ($separator) {
+            echo sprintf('<li>%s%s%s</li>', esc_html($key), esc_html($separator), esc_html($value));
+        });
         echo '</ul></div>';
-    }
-
-    listarArray($changelog, $separator);
+	}
+	
+	listarArray($changelog, $separator);
 }
 
-add_action('wp_dashboard_setup', 'add_changelog_widget');
-
-function add_changelog_widget()
+function add_changelog_widget() 
 {
-    if (current_user_can('editor' || 'admin')) {
-        wp_add_dashboard_widget(
-            'changelog_widget',
-            'Bat-Bite - Cambios',
-            'changelog_widget_callback'
-        );
-    }
+	if (current_user_can('editor' || 'admin')) 
+	{
+		wp_add_dashboard_widget(
+			'changelog_widget',
+			'Bat-Bite - Cambios',
+			'changelog_widget_callback'
+		);
+	}
 }
+
+add_action( 'wp_dashboard_setup', 'add_changelog_widget' );
